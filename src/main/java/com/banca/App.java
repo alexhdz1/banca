86f57e42ddde77d3d1cef9ef0ea6aa6c6ded1a5c
Gazmatem <gazmatem@ciencias.unam.mx>
2021-09-03 17:30:37 -0500

    última versión

--- src/main/java/com/banca/App.java
+++ src/main/java/com/banca/App.java
@@ -5,9 +5,11 @@ import com.opencsv.exceptions.CsvException;
 
 import java.io.IOException;
 /**
- * Hello world!
- */
-
+ * Una clase para representar la cuenta bancaria de una persona
+ * con sus atributos Y métodos principales (descritas en el PDF)
+ * @version 1.0 3-Sep-2021
+ * @author Gibran Aguilar Zuñiga
+*/
 public final class App {
     private App() {
     }    
